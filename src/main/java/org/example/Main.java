aabaa0ac2980c5b5bc17dac867fa1740c02df7c6
Aritz <herreroaritz@gmail.com>
2024-09-12 14:11:34 +0200

    FIX nueva linea corregida por tilde

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,19 @@
+/**
+ * Clase de prueba del proyecto
+ */
+
+
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//todo cambiar de descripción
 public class Main {
+    /**
+     * Procedimiento main trivial
+     * @param args parametros de entrada vacios
+     */
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        //salida estandar
         System.out.println("Hello world!");
+
     }
 }
